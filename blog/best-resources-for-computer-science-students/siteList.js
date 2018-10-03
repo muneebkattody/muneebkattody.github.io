163b13a7df35df91156768fbda6f84b9d2e257e4
muneebkattody <muneebkattody@users.noreply.github.com>
2018-10-03 06:18:42 +0530

    Day11

---
+++ blog/best-resources-for-computer-science-students/siteList.js
@@ -0,0 +1,94 @@
+<script>
+	var siteData = {
+		'ideas': {
+			{
+				'url': 'https://dev.to/',
+				'desc': 'Where programmers share ideas and help each other grow. It is an online community for sharing and discovering great ideas, having debates, and making friends. Anyone can share articles, questions, discussions, etc. as long as they have the rights to the words they are sharing. Cross-posting from your own blog is welcome.',
+				'about': 'https://dev.to/about'
+			},
+			{
+				'url': 'https://github.com/explore',
+				'desc': '',
+				'about': ''
+			}
+		}
+		'news': {
+			{
+				'url': 'https://news.ycombinator.com/'
+				'desc': 'Hacker News is a social news website focusing on computer science and entrepreneurship. It is run by Paul Graham&apos;sinvestment fund and startup incubator, Y Combinator. In general, content that can be submitted is defined as "anything that gratifies one&apos;s intellectual curiosityWhere programmers share ideas and help each other grow. It is an online community for sharing and discovering great ideas, having debates, and making friends. Anyone can share articles, questions, discussions, etc. as long as they have the rights to the words they are sharing. Cross-posting from your own blog is welcome.',
+				'about': ''
+			},
+			{
+				'url': 'https://www.techmeme.com/',
+				'desc': 'Techmeme is a technology news aggregator. The website has been described as "a one-page, aggregated, filtered,archiveable summary in near real-time of what is new and generating conversation".',
+				'about': 'https://en.wikipedia.org/wiki/Techmeme'
+			},
+			{
+				'url': 'https://slashdot.org/',
+				'desc': 'Slashdot (sometimes abbreviated as /.) is a social news website that originally billed itself as "News for Nerds. Stuff that Matters". It features news stories on science, technology, and politics that are submitted and evaluated by site users and editors. Each story has a comments section attached to it where users can add online comments. The website was founded in 1997 by Hope College students Rob Malda, also known as "CmdrTaco", and classmate Jeff Bates, also known as "Hemos". In 2012, they sold it to[4] DHI Group, Inc. (i.e., Dice Holdings International, which created the Dice.com website for tech job seekers[5][6]). In January 2016, BizX acquired Slashdot Media, including both slashdot.org and SourceForge.',				
+				'about': 'https://en.wikipedia.org/wiki/Slashdot'
+			}
+		}
+		'qanda': {
+			{
+				'url': 'https://www.quora.com/',
+				'desc': 'Quora is a question-and-answer site where questions are asked, answered, edited, and organized by its community of users in the form of opinions. Its publisher, Quora Inc., is based in Mountain View, California. The company was founded in June 2009, and the website was made available to the public on June 21, 2010.[5] Users can collaborate by editing questions and suggesting edits to answers that have been submitted by other users.[6] In September 2018, Quora reported hitting 300 million monthly users.[7] Users on Quora are known for written long, blog post-like answers.',
+				'about': 'https://en.wikipedia.org/wiki/Quora'
+			},
+			{
+				'url': 'https://stackoverflow.com/',
+				'desc': 'Stack Overflow is a privately held website, the flagship site of the Stack Exchange Network,[4][5][6] created in 2008 by Jeff Atwood and Joel Spolsky.[7][8] It features questions and answers on a wide range of topics in computer programming.[9][10][11] It was created to be a more open alternative to earlier question and answer sites such as Experts-Exchange. The name for the website was chosen by voting in April 2008 by readers of Coding Horror, Atwood&apos;s popular programming blog.',
+				'about': 'https://en.wikipedia.org/wiki/Stack_Overflow'
+			}
+		}
+	}
+</script>
+
+
+
+
+
+
+
+https://education.github.com/
+
+https://see.stanford.edu/
+
+https://ocw.mit.edu/courses/#electrical-engineering-and-computer-science
+https://www.w3schools.com/
+https://www.codecademy.com/learn
+https://aws.amazon.com/
+https://cloud.google.com/
+https://www.coursera.org/
+https://www.udacity.com/
+https://www.edx.org/
+https://www.khanacademy.org/
+http://ocw.mit.edu/index.htm
+https://ocw.mit.edu/index.htm
+https://www.hackerrank.com/
+https://www.topcoder.com/
+http://www.usaco.org/
+https://icpc.baylor.edu/
+https://www.wikipedia.org/
+https://arstechnica.com/
+
+https://arstechnica.com/
+http://www.learnpython.org/
+http://www.tutorialspoint.com/
+http://codeforces.com/
+
+http://www.forbes.com/
+http://noexcuselist.com/
+https://www.geeksforgeeks.org/
+https://www.codechef.com/
+http://www.cs4fn.org/
+https://www.wikibooks.org/
+https://www.lynda.com/
+http://www.freecodecamp.com/
+https://www.codeschool.com/
+https://www.computingatschool.org.uk/https://www.codewars.com/
+https://dash.generalassemb.ly/
+https://www.theodinproject.com/home
+https://www.codeconquest.com/
+
+-->
