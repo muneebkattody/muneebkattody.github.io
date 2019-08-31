b85cd83d0fd33d3a56f55e519423dc739298f781
Muneeb Kattody <31755549+muneebkattody@users.noreply.github.com>
2019-08-31 12:47:15 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,5 @@
 "use strict";
 
-$(window).on("load", function() {
-  /*------------------
-		Preloder
-	--------------------*/
-  $(".loader").fadeOut();
-  $("#preloder")
-    .delay(400)
-    .fadeOut("slow");
-});
 
 (function($) {
   /*------------------
